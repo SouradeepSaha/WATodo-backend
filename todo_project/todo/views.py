6da9592c73660e7b3443112a478e7315a5ff2133
SnailKnightHH <65925174+SnailKnightHH@users.noreply.github.com>
2022-10-19 20:17:00 -0400

    more member endpoints created

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -29,4 +29,24 @@ def MemberList(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-            
+
+
+@api_view(['GET', "PUT", "DELETE"])
+def OneMember(request, uid):
+    try:
+        mem = Member.objects.get(pk=uid)
+    except Member.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MemberSerializer(mem)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MemberSerializer(mem, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        mem.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -20,5 +20,6 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('home/', include('todo.urls')),
-    path('members/', views.MemberList)
+    path('members/', views.MemberList),
+    path('members/<int:uid>', views.OneMember),
 ]
