da0ce2830c8c63e500a02ab82dcc7dcf6004c856
SnailKnightHH <65925174+SnailKnightHH@users.noreply.github.com>
2022-12-08 17:36:35 -0500

    update test production and sample data

--- tests/test-sample.sql
+++ tests/test-sample.sql
@@ -3,19 +3,15 @@ INSERT INTO Users (username, email, password, name) VALUES
 ("johnsmith", "johnsmith@gmail.com", PasS@Wor93v, "John Smith");
 
 --feature 2: check whether user with credentials exists at log in
-SELECT Verified FROM cs348.Users WHERE EXISTS (
 SELECT * FROM Users
-WHERE email = “andrewdawson178908”);
-
-SELECT * FROM cs348.Users
-WHERE email = “andrewdawson178908” AND password = “hashedPassword”;
+WHERE email = "elijones662283@gmail.com" AND password = "$2a$08$yPa4CGd1Injn8ebfJjQeoeAWAXqcoh4ggZ79FG5YuH15xZ.ZkEc.m";
 
 --feature 3: delete tasks
-DELETE FROM cs348.Tasks WHERE task_id= "1435";
+DELETE FROM cs348.Tasks WHERE task_id= "60";
 
---feature 4: update the user in
-UPDATE cs348.Tasks SET task_name = 'modified task name' WHERE task_id = 1435;
-UPDATE cs348.Tasks SET task_name = 'modified task name' WHERE task_id = 1436;
+--feature 4: update the task in Tasks
+UPDATE cs348.Tasks SET task_name = 'modified task name' WHERE task_id = 123;
+UPDATE cs348.Tasks SET task_name = 'modified task name' WHERE task_id = 124;
 
 --feature 5: display tasks with specific tag
 create view TasksWithTag AS
